7383fa54a5d82178533eb3e3de0235ae4cbc0e4a
김서연 <ksyssyy070814@gmail.com>
2024-09-26 11:57:29 +0900

    CSS grid 연습.calendar js:이전 달 뒷날짜, 다음 달 앞날짜 보여주자

--- 240829_grid 연습/calendar.js
+++ 240829_grid 연습/calendar.js
@@ -34,6 +34,8 @@ const changeMonth = (diff) => {
 
     // js 변수에 innerHTML = `${year}년 ${month + 1}월`
     calendarHeaderH1.innerHTML = `<i>${year}년 ${month + 1}월</i>`
+    // 달력 새로 그리자
+    setCalendar(currentDate);
 
 
 }
@@ -42,12 +44,18 @@ const setCalendar = (date) => {
 
     // 현재년
     const year = date.getFullYear();
-
     // 현재월
     const month = date.getMonth();
     // 이번 달 마지막 날짜
     const lastDate = new Date(year, month + 1, 0);   // 다을달 1일의 전 날 => 현재년, 현재월 + 1, 1 - 1
     const lastDateDate = lastDate.getDate();
+    // 이번 달 마지막 날짜의 요일
+    const lastDay = lastDate.getDay();      //요일
+    // 이번 달 마지막 날짜
+    const prevMonthLastDate = new Date(year, month, 0); //이번 달 1일의 전 날
+    const prevMonthLastDateDate = prevMonthLastDate.getDate();
+    // 이번 달 첫날의 모임
+    const firstDay = new Date(year, month, 1).getDay();
 
     // let weelNameString = `<div class="item week-name">일</div>
     // <div class="item week-name">월</div>
@@ -68,18 +76,32 @@ const setCalendar = (date) => {
 
     calendarContainerDiv.innerHTML = weekNameString;
 
-    // 이전 달의 뒷날짜 표시하자
+    // 이전 달의 뒷날짜 표시하자(?~이전 달 마지막 날짜 ? : 이전 달 마지막 날짜-이번 달 첫날의 요일+1)
+    for (let date = prevMonthLastDateDate - firstDay + 1; date <= prevMonthLastDateDate; date++) {
+        let currentMonthDateDiv = document.createElement("div");        // <div></div>
+        currentMonthDateDiv.className = "item other-month";             // <div class = "item other-month"></div>
+        currentMonthDateDiv.textContent = date;                         // <div class = "item other-month">날짜</div>
+        calendarContainerDiv.appendChild(currentMonthDateDiv);          // <div id = "calendar-container"><div class = "item other-month">날짜</div></div>
 
-    // 이번 달의 모든 날짜 표시하자
-    for (let date = 1; date <= lastDateDate; date++) {
-        let currentMonthDateDiv = document.createElement("div");
-        currentMonthDateDiv.className = "item";
-        currentMonthDateDiv.textContent = date;
-        calendarContainerDiv.appendChild(currentMonthDateDiv);
-        
     }
 
-    // 다음 달의 앞날짜 표시하자
+    // 이번 달의 모든 날짜 표시하자(1~이번 달 마지막 날짜)
+    for (let date = 1; date <= lastDateDate; date++) {
+        let currentMonthDateDiv = document.createElement("div");        // <div></div>
+        currentMonthDateDiv.className = "item";                         // <div class = "item"></div>
+        currentMonthDateDiv.textContent = date;                         // <div class = "item">날짜</div>
+        calendarContainerDiv.appendChild(currentMonthDateDiv);          // <div id = "calendar-container"><div class = "item">날짜</div></div>
+
+    }
+
+    // 다음 달의 앞날짜 표시하자(1~? ?: 6~이번 달 마지막 날짜의 요일)
+    for (let date = 1; date <= 6 - lastDay; date++) {
+        let currentMonthDateDiv = document.createElement("div");        // <div></div>
+        currentMonthDateDiv.className = "item other-month";             // <div class = "item other-month"></div>
+        currentMonthDateDiv.textContent = date;                         // <div class = "item other-month">날짜</div>
+        calendarContainerDiv.appendChild(currentMonthDateDiv);          // <div id = "calendar-container"><div class = "item other-month">날짜</div></div>
+
+    }
 
 }
 
