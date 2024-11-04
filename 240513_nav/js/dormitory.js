4b9ab10f05642c521a83eb4fc010508ca7289d06
김서연 <ksyssyy070814@gmail.com>
2024-11-04 15:06:23 +0900

    기숙사 필요한 초기 데이터 요청해서 응답 => js 변수

--- 240513_nav/js/dormitory.js
+++ 240513_nav/js/dormitory.js
@@ -13,17 +13,53 @@ const pageDivs = [calendarDiv, selectionWashingmachineTimeDiv, selectionRoomName
 // selectionRoomNameDiv.style.display = "block";
 // boardDiv.style.display = "block";
 
+let allData; // 모든 초기화 정보: 세탁기, 시간, 호실 정보
+let weeklyReservation; // 미리 요일별로 지정된 예약 정보
+let newReservation; // 사용자가 입력하고 있는 예약 정보
+let reservation; // 사용자가 예약 완료한 정보들
+
+const initDate = async () => {
+    // allData 가져오자
+    const getAllData = async (url) => {
+        return fetch(url)
+        .then(response => response.json())
+        .then(data => data)
+        .catch(error => console.error(error.message));
+    }
+
+    // weeklyReservation 가져오자
+    const getWeeklyReservation = async (url) => {
+        try {
+            const response = await fetch(url);
+            const data = await response.json();
+            return data;
+        } catch(error){
+            console.error(error.message);
+        }
+    }
+
+    allData = await getAllData("js/allData.json");
+    console.log(allData);
+
+
+    weeklyReservation = await getWeeklyReservation("js/weekly-reservation.json");
+    console.log(weeklyReservation);
+
+
+}
+
 const setPage = (page) => {
+
     // clear select
-    for(const selectionItemDiv of selectionItemDivs){
+    for (const selectionItemDiv of selectionItemDivs) {
         selectionItemDiv.classList.remove("select")
     }
     // selectionItemDivs.forEach(selectionItemDiv => {
     // })
 
     //select selection
-    if(selectionItemDivs.length >= page){       // 4페이지 selection은 없음
-        selectionItemDivs[page-1].classList.add("select");
+    if (selectionItemDivs.length >= page) {       // 4페이지 selection은 없음
+        selectionItemDivs[page - 1].classList.add("select");
     }
 
     // clear pages
@@ -31,7 +67,8 @@ const setPage = (page) => {
         pageDiv.style.display = "none";     // 모든 페이지 안 보이게 하자
     })
     // show page
-    pageDivs[page-1].style.display = "block";       // 1페이지: calendar, 2페이지: swt, 3페이지: srn, 4페이지: board
+    pageDivs[page - 1].style.display = "block";       // 1페이지: calendar, 2페이지: swt, 3페이지: srn, 4페이지: board
 }
 
+initDate();
 setPage(1);
