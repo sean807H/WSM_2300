4ffb70638fd3b2e0d5e686a0a3d82bba56f27ef6
김서연 <ksyssyy070814@gmail.com>
2024-11-07 13:07:42 +0900

    기숙사 세탁기 번호 표시하자

--- 240513_nav/js/dormitory.js
+++ 240513_nav/js/dormitory.js
@@ -7,6 +7,8 @@ const selectionWashingmachineTimeDiv = document.getElementById("selection-washin
 const selectionRoomNameDiv = document.querySelector("#selection-room-name");
 const boardDiv = document.querySelector("#board");
 const pageDivs = [calendarDiv, selectionWashingmachineTimeDiv, selectionRoomNameDiv, boardDiv];
+const washingmachineSelect = document.getElementById("washingmachine");
+const timeSelect = document.querySelector("#time");
 
 // calendarDiv.style.display = "block";
 // selectionWashingmachineTimeDiv.style.display = "block";
@@ -22,9 +24,9 @@ const initDate = async () => {
     // allData 가져오자
     const getAllData = async (url) => {
         return fetch(url)
-        .then(response => response.json())
-        .then(data => data)
-        .catch(error => console.error(error.message));
+            .then(response => response.json())
+            .then(data => data)
+            .catch(error => console.error(error.message));
     }
 
     // weeklyReservation 가져오자
@@ -33,7 +35,7 @@ const initDate = async () => {
             const response = await fetch(url);
             const data = await response.json();
             return data;
-        } catch(error){
+        } catch (error) {
             console.error(error.message);
         }
     }
@@ -64,21 +66,47 @@ const setPage = (page) => {
     })
     // show page
     pageDivs[page - 1].style.display = "block";       // 1페이지: calendar, 2페이지: swt, 3페이지: srn, 4페이지: board
+
+    // 2페이지
+    if (page === 2) {     // 세탁기, 시간
+        // 1,2,3번 세탁기, 1,2,3 시간 초기화
+        let allWashingmachineTime = {"1":["1","2","3"],"2":["1","2","3"],"3":["1","2","3"]};
+        // 클릭한 날짜의 요일 구하자
+        // 미리 예약된 예약을 보고, 예약된 세탁기와 예약된 시간이 있으면 초기화 항목에서 빼자
+        // 사용자가 예약한 예약을 보고, 예약된 세탁기와 예약된 시간이 있으면 초기화 항목에서 빼자
+        // 초기화 항목에서 예약된 시간 뺀 후, 모든 시간이 없는 세탁기는 빼자
+        // 세탁기 select에 option 만들어 넣자
+        let washingmachines = Object.keys(allWashingmachineTime);   // ["1","2","3"]
+        washingmachines.forEach((washingmachine) => {
+            let newOption = document.createElement("option");   // <option></option>
+            newOption.value = washingmachine;   // <option value="세탁기번호"></option>
+            newOption.textContent = `${washingmachine}번 세탁기`;   // <option value="세탁기번호">세탁기번호번 세탁기</option>
+            washingmachineSelect.appendChild(newOption);    //washingmachineSelect에 자식으로 넣자
+        });
+        // 시간 select에 option 만들어 넣자
+        // [다음] 클릭 => 세탁기번호, 시간 번호를 보관하자 => setPage(3)
+
+
+    } else if (page === 3) {      // 호실, 이름
+
+    } else if (page === 4) {      // 세탁기 예약 현황표
+
+    }
 }
 
 const clickDate = (event) => {
     console.log(event.target.dataset.date);     // <div class="item" data-date="뭐시기">텍스트</div> => 뮈시기
     newReservation = {      // 날짜, 세탁기, 시간, 호실, 이름, 알림
-        "date":undefined,
-        "washingmachine":undefined,
-        "time":undefined,
-        "room":undefined,
-        "name":undefined,
-        "notification":undefined
+        "date": undefined,
+        "washingmachine": undefined,
+        "time": undefined,
+        "room": undefined,
+        "name": undefined,
+        "notification": undefined
     }
     newReservation.date = event.target.dataset.date;        //클릭한 날짜 정보 세 예약에 기록하자
     setPage(2)      // 2페이지로 이동하자
-    
+
 }
 
 initDate();
